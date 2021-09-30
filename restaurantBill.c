2b0e65dfb0cc5a0b8fc8d0b2a174e288face2fae
Christian Badoy <clbadoy@cpp.edu>
2021-09-30 05:23:54 -0700

    Initial commit for restaurantBill.

---
+++ restaurantBill.c
@@ -0,0 +1,9 @@
+/**
+ * @file            restaurantBill.c
+ * @author          Christian Badoy (clbadoy@cpp.edu)
+ * @professor       Professor Davarpanah
+ * @brief The       purpose of this program is to calcilate a restaurant bill with a tax rate
+ *                  and a tip rate of the user's preference.
+ * @version         0.1
+ * @date            2021-09-30
+ */
